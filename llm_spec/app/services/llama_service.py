aa0f86ae41a9eab93ec42613a05a8fee801dfbbc
Lucas Carvalho <lucasddmc@outlook.com>
2025-04-07 23:38:43 -0300

    fix

--- llm_spec/app/services/llama_service.py
+++ llm_spec/app/services/llama_service.py
@@ -95,7 +95,7 @@ class LlamaService(LLMServiceInterface):
         Accepts a prompt string and returns the generated response using the Ollama model.
         """
         try:
-            print("startou")
+            # print("startou")
             # logger.debug(f"Sending prompt to Ollama: {prompt}")
             # response = ollama.chat(
             #     model=self.model_name,
@@ -108,14 +108,14 @@ class LlamaService(LLMServiceInterface):
                     'content': prompt,
                 },
             ])
-            print(response['message']['content'])
+            # print(response['message']['content'])
             # or access fields directly from the response object
-            print(response.message.content)
-            print("recebi")
+            # print(response.message.content)
+            # print("recebi")
 
             reply = response["message"]["content"]
             logger.debug(f"Received response from Ollama: {reply}")
-            print("reply")
+            # print("reply")
             return reply
         except Exception as e:
             logger.error(f"Error during Ollama message generation: {e}")
